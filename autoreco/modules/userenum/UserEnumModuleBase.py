2784b604799ca32ae20348f8e31af1a6ada46d14
bmigette <bastien.migette@gmail.com>
2024-02-12 13:54:04 +0100

    Added enum user module base

--- autoreco/modules/userenum/UserEnumModuleBase.py
+++ autoreco/modules/userenum/UserEnumModuleBase.py
@@ -3,6 +3,7 @@ from ...State import State
 import os
 
 class UserEnumModuleBase(ModuleInterface):
-    def __init__(self, testid, target, module_name, args={}):
-        self._baselogdir = os.path.join(State().TEST_WORKING_DIR, "userenum")
+    def __init__(self, testid, target, module_name, args={}):        
         super().__init__(self, testid, target, module_name, args)
+        self._baselogdir = os.path.join(State().TEST_WORKING_DIR, "userenum")
+        self._usehostinlogdir = False
